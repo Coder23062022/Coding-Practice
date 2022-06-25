3cd99543868674b72b4dd99c8688e9a804b998da
Coder23062022 <krishban22@gmail.com>
2022-06-25 20:03:14 +0530

    Code commit

--- Krish/src/Array/ThreeSum.java
+++ Krish/src/Array/ThreeSum.java
@@ -7,6 +7,8 @@ import java.util.List;
 //Problem: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k,
 //and nums[i] + nums[j] + nums[k] == 0.
 
+//Reference video: https://www.youtube.com/watch?v=qJSPYnS35SE&t=468s&ab_channel=NickWhite
+
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
