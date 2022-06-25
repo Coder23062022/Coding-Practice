3faee9fccb2777de4b0520f72131f4324a9045ae
Coder23062022 <krishban22@gmail.com>
2022-06-25 13:20:15 +0530

    New code added

---
+++ Krish/src/Array/TopKFrequentElements.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.*;
+
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] result = topKFrequentElements(nums, k);
+        System.out.println("Top " + k + " Frequent Elements are:");
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static int[] topKFrequentElements(int[] nums, int k) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            if (hm.containsKey(num)) {
+                hm.put(num, hm.get(num) + 1);
+            } else {
+                hm.put(num, 1);
+            }
+            //Here map will be like: 1 - 3, 2 - 2, 3 - 1
+        }
+
+        List<Integer> list = new ArrayList<>(hm.values());
+        Collections.sort(list); //List will be: 1,2,3
+
+        //Finding the kth last element in the sorted list so that all the elements in the map having that frequency or more than
+        //that will come into the result.
+        int min_freq = list.get(list.size() - k); //min_freq will be: list.get(3-2) = list.get(1) = 2
+
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            //All the elements in the map having min_freq or more than that will come into the result.
+            if (entry.getValue() >= min_freq) {
+                result.add(entry.getKey());
+            }
+        }
+        return result.stream().mapToInt(x -> x).toArray();
+    }
+}
---
+++ Krish/src/Strings/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+//        String[] strs = {"cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc"};
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String originalString : strs) {
+            char[] ca = originalString.toCharArray();
+            Arrays.sort(ca); //sort the string lexicographically.
+            String sortedString = String.valueOf(ca);
+
+            if (!map.containsKey(sortedString))
+                //If map doesn't contain the sorted string, then create a new list for that sorted string as the value of the map.
+                //For first case - key: aet, value: {}
+                map.put(sortedString, new ArrayList<>());
+
+            //Adding the original string to the existing list associated with the same sorted string.
+            // For first case - key: aet, value: {eat}
+            map.get(sortedString).add(originalString);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
